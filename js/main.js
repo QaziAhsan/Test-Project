f3b9ba4ae597eed6d90e5a3e41b47606ca236245
QaziAhsan <iamahsan96@gmail.com>
2020-07-21 14:26:06 +0500

    final-changes

--- js/main.js
+++ js/main.js
@@ -8,16 +8,19 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
+            items:2
+        },
+        1400:{
             items:3
         },
      
     }
 })
 
-
+/**************Header*************/
 
     document.querySelector('.bullet').addEventListener('click', (e) => {
         document.querySelector('.nav_links').classList.toggle('active');
@@ -25,3 +28,31 @@ $('.owl-carousel').owlCarousel({
       });
       
 
+/**************Form*************/
+
+      const form = document.querySelector('#form');
+      const mail = document.querySelector('#mail');
+      
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+      
+        if (mail.value === '') {
+          mail.parentElement.classList.add('error');
+        }else {
+          mail.parentElement.classList.remove('error');
+          if(mail.type == 'email'){
+            if (validateEmail(mail.value)) {
+              mail.parentElement.classList.remove('error');
+            }else {
+              mail.parentElement.classList.add('error');
+            }
+          }
+        }
+      })
+
+      function validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      } 
+      
+      
